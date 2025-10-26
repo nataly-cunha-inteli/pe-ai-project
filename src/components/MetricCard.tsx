aa3cfbb76b3d554616d42a5d648c8a1a917d749b
nataly-cunha-inteli <158575982+nataly-cunha-inteli@users.noreply.github.com>
2025-10-25 23:07:28 -0300

    ajusta cards da dashboard

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -7,24 +7,25 @@ interface MetricCardProps {
   iconAlt?: string;
 }
 
-export const MetricCard: React.FC<MetricCardProps> = ({ 
-  icon, 
-  label, 
-  value, 
-  iconAlt = label 
+export const MetricCard: React.FC<MetricCardProps> = ({
+  icon,
+  label,
+  value,
+  iconAlt = label,
 }) => {
   return (
-    <article className="min-w-[200px] border bg-[#FBFBFB] self-stretch min-h-[101px] w-[225px] gap-2 my-auto p-6 rounded-[10px] border-solid border-[#DEE2E6] max-md:px-5">
-      <div className="relative flex w-full flex-col">
-        <div className="z-0 flex w-[108px] max-w-full items-center gap-3 text-base text-black font-normal">
+    <article className="min-w-[200px] border bg-[#FBFBFB] self-stretch min-h-[101px] w-[225px] gap-2 my-auto p-6 rounded-[10px] border-solid border-[#DEE2E6] max-md:px-5 overflow-hidden">
+      <div className="flex flex-col justify-between h-full w-full">
+        <div className="flex items-center gap-3 text-base text-black font-normal max-w-full">
           <img
             src={icon}
             alt={iconAlt}
             className="aspect-[1] object-contain w-6 self-stretch shrink-0 my-auto"
           />
-          <span className="self-stretch my-auto">{label}</span>
+          <span className="self-stretch my-auto truncate max-w-[calc(100%-36px)]">{label}</span>
         </div>
-        <div className="text-black text-[27px] font-black absolute z-0 bottom-[-39px] w-8 h-8 left-0">
+
+        <div className="text-black text-[27px] font-black mt-4 break-words truncate">
           {value}
         </div>
       </div>
